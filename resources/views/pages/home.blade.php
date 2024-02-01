646881af29294b44af7e7a80d3f62f295301cabb
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-01 16:04:26 +0100

    collegato db + creato model + in home.blade inserita card da bootstrap

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,21 @@
 
 @section('main-content')
     <h1>
-        Home
+        Lista Film:
     </h1>
+    <section>
+        <ul>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">An item</li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+                </ul>
+            </div>
+        </ul>
+    </section>
 @endsection
